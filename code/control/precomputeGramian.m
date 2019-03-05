d38c5960ba7c412c393f95dfb032629203f63c13
Eli Cornblath <Eli.Cornblath@uphs.upenn.edu>
2019-03-05 17:34:23 -0500

    split gramian computation into multiple jobs

--- code/control/precomputeGramian.m
+++ code/control/precomputeGramian.m
@@ -4,17 +4,20 @@ masterdir = fullfile(basedir,'results',name_root);
 savedir = fullfile(basedir,'data','Gramians');
 mkdir(savedir);
 
-% pre-compute inverse gramian for each subject
+% pre-compute inverse gramian for each subject in groups of 40 subjects
 load(fullfile(datadir,['VolNormSC',num2str(lausanneScaleBOLD),'.mat']));
 
+n_per_split = 40;	% 40 subjects per job
+subj_range = (1+n_per_split*(split-1)):(n_per_split*split);
+subj_range(subj_range > nobs) = [];		% truncate subj_range to equal total number of subjects
 T = 1; %control horizon
-WcI = zeros(nparc,nparc,nobs);
-parpool(maxNumCompThreads-1);
-parfor N = 1:nobs
+for N = 
+	tic
 	disp(['Subject ',num2str(N)])
 	A = SCvolnorm{N};
 	Anorm = (A / max(eigs(A,1))) - eye(length(A));	% normalize A to max eig --> new max eig = 1 --> one dominant eigenmode stationary over time
-	WcI(:,:,N) = GRAMIAN(Anorm,T,false);
+	WcI_subj = GRAMIAN(Anorm,T,false);
+	save(fullfile(savedir,['GramianInverse',num2str(lausanneScaleBOLD),'Subject',num2str(N),'.mat']),'WcI_subj');
+	toc
 end
 
-save(fullfile(savedir,['GramianInverse',num2str(lausanneScaleBOLD),'.mat']),'WcI');
