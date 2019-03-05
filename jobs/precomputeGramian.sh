49d0b6a566e22be0573973e338ad7111b79ebae0
Eli Cornblath <Eli.Cornblath@uphs.upenn.edu>
2019-03-05 17:41:32 -0500

    split up precomputation of gramian

--- jobs/precomputeGramian.sh
+++ jobs/precomputeGramian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-$MP -nodisplay -r "addpath(genpath('$BD')); name_root = char('$D'); basedir = char('$BD'); cd(basedir); run([basedir,'code/control/precomputeGramian.m']); exit"
+$MP -nodisplay -r "addpath(genpath('$BD')); name_root = char('$D'); nsplits = [$NSPLITS]; split = [$S]; basedir = char('$BD'); cd(basedir); run([basedir,'code/control/precomputeGramian.m']); exit"
