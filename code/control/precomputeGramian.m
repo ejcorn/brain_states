b7d69c14641845ee3eb177f972aaca2c2e541131
Eli Cornblath <Eli.Cornblath@uphs.upenn.edu>
2019-03-05 17:37:09 -0500

    split gramian computation over multiple jobs...did it better

--- code/control/precomputeGramian.m
+++ code/control/precomputeGramian.m
@@ -4,12 +4,12 @@ masterdir = fullfile(basedir,'results',name_root);
 savedir = fullfile(basedir,'data','Gramians');
 mkdir(savedir);
 
-% pre-compute inverse gramian for each subject in groups of 40 subjects
+% pre-compute inverse gramian for each subject in groups of subjects
 load(fullfile(datadir,['VolNormSC',num2str(lausanneScaleBOLD),'.mat']));
 
-n_per_split = 40;	% 40 subjects per job
+n_per_split = ceil(nobs/nsplits);	% calculate subjects per job based on # of jobs (nsplits) and # of subjects (nobs)
 subj_range = (1+n_per_split*(split-1)):(n_per_split*split);
-subj_range(subj_range > nobs) = [];		% truncate subj_range to equal total number of subjects
+subj_range(subj_range > nobs) = [];		% truncate subj_range to equal total number of subjects... only happens on last job
 T = 1; %control horizon
 for N = 
 	tic
