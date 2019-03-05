a5c9a958cc760aff6349c4519b9c5100d505c091
Eli Cornblath <Eli.Cornblath@uphs.upenn.edu>
2019-03-05 16:43:20 -0500

    use parallel for loop

--- code/control/precomputeGramian.m
+++ code/control/precomputeGramian.m
@@ -1,18 +1,20 @@
 addpaths;
 load(fullfile(basedir,['data/Demographics',name_root,'.mat']));
 masterdir = fullfile(basedir,'results',name_root);
-savedir = fullfile(basedir,'data');
+savedir = fullfile(basedir,'data','Gramians');
+mkdir(savedir);
 
 % pre-compute inverse gramian for each subject
 load(fullfile(datadir,['VolNormSC',num2str(lausanneScaleBOLD),'.mat']));
 
 T = 1; %control horizon
 WcI = zeros(nparc,nparc,nobs);
-for N = 1:nobs
+parpool(maxNumCompThreads-1);
+parfor N = 1:nobs
 	disp(['Subject ',num2str(N)])
 	A = SCvolnorm{N};
 	Anorm = (A / max(eigs(A,1))) - eye(length(A));	% normalize A to max eig --> new max eig = 1 --> one dominant eigenmode stationary over time
 	WcI(:,:,N) = GRAMIAN(Anorm,T,false);
 end
 
-save(fullfile(savedir,['GramianInverse',num2str(lausanneScaleBOLD),'.mat']));
+save(fullfile(savedir,['GramianInverse',num2str(lausanneScaleBOLD),'.mat']),'WcI');
