4473df3395cb8cdb79111c13be3d4f768166f4cf
Eli Cornblath <Eli.Cornblath@uphs.upenn.edu>
2019-03-07 11:38:50 -0500

    update processing script to load files based on demographic csv subject ids, vs. just order in directory

--- code/process/ProcessDataBaumSample.m
+++ code/process/ProcessDataBaumSample.m
@@ -2,7 +2,7 @@
 
 name_root = ['Scan',scan,'Laus',num2str(lausanneScaleBOLD),'Z',num2str(zdim),extralabel];
 datadir = [basedir,'data'];
-masterdir = fullfile(basedir,'results',name_root)
+masterdir = fullfile(basedir,'results',name_root);
 demoLTN = readtable('/data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/subject_demographics/n884_pnc_dtiNbackRest_subject_demographics.csv');
 demoLTN = sortrows(demoLTN,'scanid','ascend');
 nobs = length(demoLTN.scanid); demoLTN.Sex = demoLTN.sex - 1;
@@ -28,25 +28,25 @@ elseif lausanneScaleBOLD == 125
     nparc = 233;
 end
 
-dtidir = dir(['/data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/diffusion_data/volNormSC/Lausanne',num2str(lausanneScaleBOLD),'/*.mat']);
-dtifnames = fullfile({dtidir.folder},{dtidir.name});
+dtifnames = cellstr([repmat(['/data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/diffusion_data/volNormSC/Lausanne',num2str(lausanneScaleBOLD),'/'],[nobs 1]),num2str(demoLTN.scanid),...
+    repmat(['_volNormStreamline_LausanneScale',num2str(lausanneScaleBOLD),'.mat'],[nobs 1])]);
     
-% check if structure exists
+% check if structure exists for every subject -- this removes 4 subjects in 462 node scale
 StructureExist = logical(cellfun(@(x) exist(x,'file'),dtifnames));
 demoLTN = demoLTN(StructureExist,:);
+if sum(StructureExist) < nobs
+    disp(['removing ',num2str(nobs-sum(StructureExist)),' subject(s) with missing structure'])
+end
 nobs = sum(StructureExist);
 SCvolnorm = cell(nobs,1);
-dtidir = dir(['/data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/diffusion_data/volNormSC/Lausanne',num2str(lausanneScaleBOLD),'/*.mat']);
-dtifnames = fullfile({dtidir.folder},{dtidir.name});
+dtifnames = cellstr([repmat(['/data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/diffusion_data/volNormSC/Lausanne',num2str(lausanneScaleBOLD),'/'],[nobs 1]),...
+    num2str(demoLTN.scanid),...
+    repmat(['_volNormStreamline_LausanneScale',num2str(lausanneScaleBOLD),'.mat'],[nobs 1])]);
 
-if sum(StructureExist) < nobs
-    disp('structure exclusion failed')
-    return
-end
-
+% iterate through subjects, store structural connectivity matrices in a cell
 for N = 1:nobs
     load(dtifnames{N});
-    SCvolnorm{N} = volNorm_connectivity(1:nparc,1:nparc);    
+    SCvolnorm{N} = volNorm_connectivity(1:nparc,1:nparc);  % remove brainstem  
 end
 
 disp('SC loaded');
@@ -58,14 +58,25 @@ restNumTRs = 120; nbackNumTRs = 225;
 restTS = nan(restNumTRs,nparc,nobs);
 nbackTS = nan(nbackNumTRs,nparc,nobs);
 
+restfnames = [repmat(['data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/rest_data/Lausanne',num2str(lausanneScaleBOLD),'/'],[nobs 1]),...
+    num2str(demoLTN.scanid),...
+    repmat(['_Lausanne',num2str(lausanneScaleBOLD),'_ts.1D'],[nobs 1])];
+    restfnames = [repmat(['data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/nback_data/Lausanne',num2str(lausanneScaleBOLD),'/'],[nobs 1]),...
+    num2str(demoLTN.scanid),...
+    repmat(['_Lausanne',num2str(lausanneScaleBOLD),'_ts.1D'],[nobs 1])];
+nbackfnames = [repmat(['/data/jag/bassett-lab/Lausanne1601/nback/nbackNetwork_Lausanne',num2str(lausanneScaleBOLD),'/','Lausanne',num2str(lausanneScaleBOLD),'Timeseries/'],[nobs 1]),num2str(demoLTN.scanid),repmat(['_Lausanne',num2str(lausanneScaleBOLD),'_ts.1D'],[nobs 1])];
+
 restdir = dir(['/data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/rest_data/Lausanne',num2str(lausanneScaleBOLD),'/*.1D']);
 restfnames = fullfile({restdir.folder},{restdir.name});
 nbackdir = dir(['/data/tesla-data/ecornblath/PNCLatestFreeze/cornblathPncRestNback/nback_data/Lausanne',num2str(lausanneScaleBOLD),'/*.1D']);
 nbackfnames = fullfile({nbackdir.folder},{nbackdir.name});
 for N = 1:nobs
+    disp(['Subject ',num2str(N)])
+    % default: time series is NaNs
     tmpr = nan(restNumTRs,nparc);
     tmpn = nan(nbackNumTRs,nparc);
     
+    % load time series and replace NaNs IF data exists
     if exist(restfnames{N},'file') && fseek(fopen(restfnames{N}), 1, 'bof') ~= -1
         tmpr = dlmread(restfnames{N});
     end
@@ -73,6 +84,7 @@ for N = 1:nobs
         tmpn = dlmread(nbackfnames{N});
     end
     
+    % z-score according to specified input, if desired
     if zdim == 0
         restTS(:,:,N) = tmpr(:,1:nparc);
         nbackTS(:,:,N) = tmpn(:,1:nparc);
@@ -92,6 +104,7 @@ disp('BOLD Data loaded');
 rest_missingdata = ~logical(squeeze(sum(sum(isnan(restTS),1),2)));
 nback_missingdata = ~logical(squeeze(sum(sum(isnan(nbackTS),1),2)));
 
+% will be true only if subject has data for both rest AND n-back
 BOLDMissingDataExclusionMask = and(rest_missingdata,nback_missingdata);
 
 restTS = restTS(:,:,BOLDMissingDataExclusionMask);
