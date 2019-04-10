c252beaf2d9e050522ad38be249b426e2d90d9bb
Eli Cornblath <Eli.Cornblath@uphs.upenn.edu>
2019-04-10 09:11:08 -0400

    ipr null centroids

--- code/assesscluster/iprnullcentroids.m
+++ code/assesscluster/iprnullcentroids.m
@@ -13,8 +13,34 @@ for K = 1:numClusters
 end
 
 clusterNames = cellstr(num2str([1:numClusters]'));
-clusterNamesUp = cellstr(strings(numClusters,1));
-clusterNamesDown = cellstr(strings(numClusters,1));
-
+clusterNamesUp = repmat({' '},numClusters,1);
+clusterNamesDown = repmat({' '},numClusters,1);
 
 save(fullfile(savedir,['CentroidsIPRNull_k',num2str(numClusters),'.mat']),'kClusterCentroids','clusterNames','clusterNamesUp','clusterNamesDown');
+
+load([masterdir,'/clusterAssignments/k',num2str(numClusters),name_root,'.mat'])
+PNCcentroids = clusterAssignments.(['k',num2str(numClusters)]).bestCentroid;
+PNCnames = clusterAssignments.(['k',num2str(numClusters)]).clusterNames;
+PNCvsIPR = corr(kClusterCentroids,PNCcentroids);
+[~,shuffleIdx] = max(PNCvsIPR,[],1);
+
+if length(unique(shuffleIdx)) == length(shuffleIdx)
+	IPR_centroidsPNCorder = kClusterCentroids(:,shuffleIdx);
+else
+	IPR_centroidsPNCorder = kClusterCentroids;
+end
+
+f = figure;
+imagesc(PNCvsIPR(shuffleIdx,:)); colormap('plasma');
+ylabel('IPR Null'); xlabel('Full Sample'); axis square
+yticks(1:numClusters); xticks(1:numClusters);
+yticklabels(clusterNames); xticklabels(PNCnames);
+xtickangle(90);
+COLOR_TICK_LABELS(true,true,numClusters);
+h=colorbar; caxis([-1 1]); h.Ticks = [-1 0 1]; h.TickLabels = [-1 0 1];
+title({'Spatial Correlation'});
+set(gca,'FontSize',8);
+f.PaperUnits = 'inches';
+f.PaperSize = [2.7 2.7];
+f.PaperPosition = [0 0 2.7 2.7];
+saveas(f,fullfile(savedir,['IPRNullSpatialCorr_k',num2str(numClusters),name_root,'.pdf']),'pdf');
