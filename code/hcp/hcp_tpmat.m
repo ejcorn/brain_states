e46f74a867b84f525169d33e2d7560c7c84a5a2c
Eli Cornblath <Eli.Cornblath@uphs.upenn.edu>
2019-04-16 09:46:53 -0400

    add new permutation testing fxn to hcp code

--- code/hcp/hcp_tpmat.m
+++ code/hcp/hcp_tpmat.m
@@ -23,9 +23,9 @@ pncnTP = mean(transitionProbability,1);
 
 % correlate TPs and PPs between HCP and PNC
 restTPsim = round(corr(pncrTP(offDiag)',hcprTP(offDiag)'),2,'significant');
-restPPsim = round(corr(pncrTP(onDiag)',hcprTP(onDiag)'),2,'significant');
+restPPsim = round(corr(pncrTP(onDiag)',hcprTP(onDiag)'),2,'significant'); disp(['Rest PP corr: ',num2str(restPPsim)]);
 nbackTPsim = round(corr(pncnTP(offDiag)',hcpnTP(offDiag)'),2,'significant');
-nbackPPsim = round(corr(pncnTP(onDiag)',hcpnTP(onDiag)'),2,'significant');
+nbackPPsim = round(corr(pncnTP(onDiag)',hcpnTP(onDiag)'),2,'significant'); disp(['n-back PP corr: ',num2str(nbackPPsim)]);
 
 % fold difference in TP and PP
 mean(pncrTP(offDiag) ./ hcprTP(offDiag))
@@ -40,28 +40,8 @@ nBackTransitionProbabilityMats = HCPnBacktransprobs;
 
 %% permute rest and n-back within subjects
 disp('start permutation testing')
-nperms = 10000;
-permutationIndex = rand(nsubjs,nperms) > 0.5;
-expectedRest_gt_nBack = zeros(nperms,numClusters,numClusters);
-expectednBack_gt_Rest = zeros(nperms,numClusters,numClusters);
-for P = 1:nperms
-    shuffleIndex = find(permutationIndex(:,P));
-    restTmp = restTransitionProbabilityMats; nBackTmp = nBackTransitionProbabilityMats;
-    
-    % Randomly switch rest and n-back entire matrices within each subject
-    restTmp(shuffleIndex,:,:) = nBackTransitionProbabilityMats(shuffleIndex,:,:);
-    nBackTmp(shuffleIndex,:,:) = restTransitionProbabilityMats(shuffleIndex,:,:);
-    
-    % Count # of times rest exceeds nback in each permutation
-    %expectedRest_gt_nBack(P,:,:) = squeeze(sum(restTmp > nBackTmp,1));
-    %expectednBack_gt_Rest(P,:,:) = squeeze(sum(restTmp < nBackTmp,1));    
-
-    % Compute amount by which mean rest exceeds mean nback 
-    expectednBack_gt_Rest(P,:,:) = squeeze(mean(nBackTmp,1) - mean(restTmp,1));
-
-end
-observednBack_gt_Rest = repmat(mean(nBackTransitionProbabilityMats,1) - mean(restTransitionProbabilityMats,1),[nperms 1 1]);
-nBack_gt_Rest_Pval = squeeze(sum(observednBack_gt_Rest > expectednBack_gt_Rest,1)) / nperms;
+nperms = 100000;
+pvals_twotail = PERM_TEST(nBackTransitionProbabilityMats,restTransitionProbabilityMats,nperms);
 
 % plot
 HCPgrpAvgRest = squeeze(mean(HCPresttransprobs,1)) .* ~eye(numClusters);
@@ -96,15 +76,19 @@ set(gca,'Fontname','arial');
 caxis([0 maxVal]); colorbar
 
 subplot(1,3,3);
-imagesc((nBack_gt_Rest_Pval.*~eye(numClusters))); colormap('plasma');
+grpAvgRest = squeeze(mean(restTransitionProbabilityMats,1));    
+grpAvgnBack = squeeze(mean(nBackTransitionProbabilityMats,1));
+
+subplot(1,3,3);
+imagesc((grpAvgnBack-grpAvgRest).*~eye(numClusters)); colormap('plasma');
 xticks(1:numClusters); xticklabels(clusterNames); xtickangle(90);
 yticks(1:numClusters); yticklabels(clusterNames); axis square
 ylabel('State at t'); xlabel('State at t + 1');
-lt = 0.05/(numClusters^2); ut = 1-lt;       % bonferroni correction
-[y,x] = find(nBack_gt_Rest_Pval.*~eye(numClusters) > ut | nBack_gt_Rest_Pval.*~eye(numClusters) < lt);
+sig_thresh = 0.05 / numClusters^2;      % bonferroni correction, for two-tailed p-values so only
+[y,x] = find(pvals_twotail.*~eye(numClusters) < sig_thresh);
 text(x-.12,y+.12,'*','Color','w');
-%h=colorbar; ylabel(h,'-log_{10}(p)'); caxis([0 log10(nperms)]); h.Ticks = [0 ut lt log10(nperms)]; h.TickLabels = [0 round(ut,2,'significant') round(lt,2,'significant') log10(nperms)];
-h = colorbar; ylabel(h,'p(n > R)'); caxis([lt ut]); h.Ticks = [lt 0.5 ut]; h.TickLabels = [round(lt,2,'significant') 0.5 round(ut,2,'significant')];
+caxis_bound = max(max(abs((grpAvgnBack-grpAvgRest).*~eye(numClusters))));
+h = colorbar; ylabel(h,'nback - rest'); caxis([-caxis_bound caxis_bound]); h.Ticks = [-caxis_bound 0 caxis_bound]; h.TickLabels = [round(-caxis_bound,2,'significant') 0 round(caxis_bound,2,'significant')];
 COLOR_TICK_LABELS(true,true,numClusters);
 title('n-back > Rest');
 set(gca,'FontSize',8);
@@ -117,13 +101,17 @@ f.PaperPosition = [0 0 8 4];
 
 saveas(f,fullfile(savedir,['HCP_XH_RestnBackTransProbs_k',num2str(numClusters),'_R',num2str(rTR),'N',num2str(nTR),name_root,'.pdf']),'pdf');
 
-[y,x] = find(diag(nBack_gt_Rest_Pval)' > ut | diag(nBack_gt_Rest_Pval)' < lt);
+grpAvgRest = squeeze(mean(restTransitionProbabilityMats,1));    
+grpAvgnBack = squeeze(mean(nBackTransitionProbabilityMats,1));
+
+[y,x] = find(diag(pvals_twotail)' < sig_thresh);
 f=figure;
-imagesc(diag(nBack_gt_Rest_Pval)');
+imagesc(diag((grpAvgnBack-grpAvgRest))');
+caxis_bound = max(max(abs(diag((grpAvgnBack-grpAvgRest)))));
 xticks([]); yticks([]);
 text(x-.12,y+.12,'*','Color','w');
-caxis([lt ut]); colormap('plasma'); %colorbar
-%h=colorbar; h.Ticks = [0 maxval]; h.TickLabels = [0, round(maxval,2,'significant')];
+caxis([-caxis_bound caxis_bound]); colormap('plasma'); %colorbar
+
 set(gca,'FontSize',8);
 set(gca,'TickLength',[0 0]);
 set(gca,'Fontname','arial');
@@ -141,7 +129,7 @@ f=figure;
 imagesc(rPP);
 xticks([]); yticks([]);
 caxis([0 maxval]); colormap('plasma'); %colorbar
-%h=colorbar; h.Ticks = [0 maxval]; h.TickLabels = [0, round(maxval,2,'significant')];
+disp(['Persistence probability n-back - rest colorbar bounds: +/-',num2str(caxis_bound)])
 set(gca,'FontSize',8);
 set(gca,'TickLength',[0 0]);
 set(gca,'Fontname','arial');
