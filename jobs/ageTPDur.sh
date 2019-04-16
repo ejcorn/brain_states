c4f661a1b1dbd7269e405f61fe85b0a076ada9d7
Eli Cornblath <Eli.Cornblath@uphs.upenn.edu>
2019-04-16 09:47:02 -0400

    fix jobs

--- jobs/ageTPDur.sh
+++ jobs/ageTPDur.sh
@@ -7,3 +7,4 @@ $RP ageDwell.R $D $K $BD
 $RP ageBlockFO.R $D $K $BD
 $RP ageBlockDwell.R $D $K $BD
 $RP ageTP.R $D $K $BD
+$RP ageTPintRN.R $D $K $BD
--- jobs/dprimeTPDur.sh
+++ jobs/dprimeTPDur.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 cd $BD'code/cognition'
 $RP dprimeTPDur.R $D $K $BD
-$RP dprimeTPscatter.R $D $K $BD
 $RP dprimeDwell.R $D $K $BD
+$RP dprimeTPscatter.R $D $K $BD
